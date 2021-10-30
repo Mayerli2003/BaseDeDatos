c325db891e183682fccc837e3126aea925e36891
Mayerli2003 <87994175+Mayerli2003@users.noreply.github.com>
2021-10-29 21:24:51 -0600

    Ejercicio clase viernes 29 de octubre 2021

--- SQLQuery2 Viernes 29 de  octubre 2021.sql
+++ SQLQuery2 Viernes 29 de  octubre 2021.sql
@@ -3,7 +3,7 @@ create database Hospital;
 
 use Hospital;
 
-/*Crear tabla Pacientes*/
+/*----------------------------Crear tabla Pacientes-----------------------------*/
 
 create table Pacientes(
 NumeroDeHistorialClinico int primary key identity (1,1) not null,
@@ -14,29 +14,47 @@ Domicilio varchar(50) not null,
 Provincia varchar(50) not null,
 codigoPostal varchar(50) not null, 
 observacionesPacientes varchar(50) not null,
-sexo varchar(50) not null
+fk_sexo int foreign key(fk_sexo) references PacientesSexo (pSexoId) not null,
+
 );
 
 /*select * from Pacientes 
 drop table PacientesSexo*/
+/*drop table PacienteTelefono*/
 
-insert into Pacientes values(456, 'Rigoberto', 'Montalvo','Las mesas','Chalatenango','CP 1301', 'Alergico a anticonvulsivos','Masculino');
-insert into Pacientes values(486, 'Ricardo', 'Martinez','El Calvario','Chalatenango','CP 1301', 'Alergico a la penicilina','Masculino');
-insert into Pacientes values(476, 'Karla', 'Lopez','Upatoro','Chalatenango','CP 1301', 'Alergica a los analgesicos','Femenino');
-insert into Pacientes values(450, 'Lila', 'Perez','La Palma','Chalatenango','CP 1301', 'Alergico a anticonvulsivos','Femenino');
-insert into Pacientes values(451, 'Judith', 'Rodrigues','El limon','Chalatenango','CP 1301', 'Alergico a la aspirina','Femenino');
-insert into Pacientes values(457, 'Azucena', 'Menjivar','El uno','Chalatenango','CP 1301', 'Alergico a la ibuprofeno','Femenino');
-insert into Pacientes values(496, 'Maria', 'Monterrosa','El dos','Chalatenango','CP 1301', 'Alergico al mani','Femenino' );
-insert into Pacientes values(406, 'Bella', 'Fuentes','El tres','Chalatenango','CP 1301', 'Alergico al naproxeno','Femenino');
-insert into Pacientes values(466, 'Mauro', 'Caballero','Moterredondo','Chalatenango','CP 1301', 'Alergico a los medicamentos de quimioterapia','Masculino');
-insert into Pacientes values(426, 'Miguel', 'Castillo','Concepcion ','Chalatenango','CP 1301', 'Alergico a la anestesia','Masculino');
-insert into Pacientes values(400, 'Angel', 'Asturias','La sierpe','Chalatenango','CP 1301', 'Alergico a anticonvulsivos','Masculino');
+
+insert into Pacientes values(456, 'Rigoberto', 'Montalvo','Las mesas','Chalatenango','CP 1301', 'Alergico a anticonvulsivos',1);
+insert into Pacientes values(486, 'Ricardo', 'Martinez','El Calvario','Chalatenango','CP 1301', 'Alergico a la penicilina',1);
+insert into Pacientes values(476, 'Karla', 'Lopez','Upatoro','Chalatenango','CP 1301', 'Alergica a los analgesicos',2);
+insert into Pacientes values(450, 'Lila', 'Perez','La Palma','Chalatenango','CP 1301', 'Alergico a anticonvulsivos',2);
+insert into Pacientes values(451, 'Judith', 'Rodrigues','El limon','Chalatenango','CP 1301', 'Alergico a la aspirina',2);
+insert into Pacientes values(457, 'Azucena', 'Menjivar','El uno','Chalatenango','CP 1301', 'Alergico a la ibuprofeno',2);
+insert into Pacientes values(496, 'Maria', 'Monterrosa','El dos','Chalatenango','CP 1301', 'Alergico al mani',2);
+insert into Pacientes values(406, 'Bella', 'Fuentes','El tres','Chalatenango','CP 1301', 'Alergico al naproxeno',2);
+insert into Pacientes values(466, 'Mauro', 'Caballero','Moterredondo','Chalatenango','CP 1301', 'Alergico a los medicamentos de quimioterapia',1);
+insert into Pacientes values(426, 'Miguel', 'Castillo','Concepcion ','Chalatenango','CP 1301', 'Alergico a la anestesia',1);
+insert into Pacientes values(400, 'Angel', 'Asturias','La sierpe','Chalatenango','CP 1301', 'Alergico a anticonvulsivos',1);
+insert into Pacientes values(300, 'Luis', 'Asturias','La sierpe','Chalatenango','CP 1301', 'Alergico a anticonvulsivos',1);
+insert into Pacientes values(200, 'Jorge', 'Asturias','La colonias','Chalatenango','CP 1301', 'Alergico a anticonvulsivos',1);
+insert into Pacientes values(100, 'Rene', 'Asturias','El amatillo','Chalatenango','CP 1301', 'Alergico a analgesicos',1);
+insert into Pacientes values(410, 'Manuel', 'Asturias','Concepcion','Chalatenango','CP 1301', 'Alergico a anestesia',1);
+insert into Pacientes values(420, 'Ernesto', 'Asturias','La nueva','Chalatenango','CP 1301', 'Alergico a anticonvulsivos',1);
+insert into Pacientes values(430, 'Daniel', 'Asturias','San Fernando','Chalatenango','CP 1301', 'Alergico a anticonvulsivos',1);
 
 select * from Pacientes;
 
+/*-----------------------------Crear la tabla Pacientes sexo---------------------------*/
+create table PacientesSexo(
+pSexoId  int identity (1,1) primary key ,
+sexo varchar(30) not null);
+
+insert into PacientesSexo values('Masculino');
+insert into PacientesSexo values('Femenino');
+select * from PacientesSexo
 
 
-/*Paciente telefono*/
+
+/*------------------------------Tabla Paciente telefono----------------------------*/
 create table PacienteTelefono(
 FK_NumeroDeHistorialClinico int foreign key(FK_NumeroDeHistorialClinico) references Pacientes (NumeroDeHistorialClinico) primary key not null,
 telefono1 varchar(30) not null,
@@ -58,15 +76,10 @@ insert into  PacienteTelefono values(11,'809-5007','709-9371', '430-4199');
 select * from PacienteTelefono;
 
 
-/*create table PacientesSexo(
-pSexoId  int identity (1,1) primary key ,
-sexo varchar(30) not null);
+/*-------------------------Tabla ingresos que conecta a medicos y pacientes------------------------------*/
 
-insert into PacientesSexo values('Masculino');
-insert into PacientesSexo values('Femenino');
-select * from PacientesSexo*/
+/*drop table Ingresos*/
 
-drop table Ingresos
 create table Ingresos(
 procedencia varchar(50) not null,
 fechaDeIngreso date not null  ,
@@ -81,45 +94,48 @@ insert into  Ingresos  values ('Chalatenango','2021-09-04',1,23,'Dieta de liquid
 insert into  Ingresos  values ('Chalatenango','2021-09-05',2,19,'Alergia a la anestesia',2, 2);
 insert into  Ingresos  values ('Chalatenango','2021-09-06',3,21,'Alergico a anticonvulsivos',3, 3);
 insert into  Ingresos  values ('Chalatenango','2021-09-07',1,20,'Dieta de liquidos',4, 4);
-insert into  Ingresos  values ('Chalatenango','2021-09-08',3,23,'Alergico a la ibuprofeno',6, 5);
-insert into  Ingresos  values ('Chalatenango','2021-09-09',1,22,'Alergico a anticonvulsivos',7, 6);
-insert into  Ingresos  values ('Chalatenango','2021-09-10',4,10,'Dieta de liquidos',8, 7);
-insert into  Ingresos  values ('Chalatenango','2021-09-11',1,16,'Alergico a la ibuprofeno',9, 8);
-insert into  Ingresos  values ('Chalatenango','2021-09-14',6,17,'Alergico al naproxeno',10, 9);
-insert into  Ingresos  values ('Chalatenango','2021-09-24',1,10,'Alergico a anticonvulsivos',11, 10);
-insert into  Ingresos  values ('Chalatenango','2021-08-04',5,30,'Dieta de liquidos',1, 11);
-insert into  Ingresos  values ('Chalatenango','2021-09-01',1,31,'Alergico a anticonvulsivos',2, 10);
-insert into  Ingresos  values ('Chalatenango','2021-09-02',1,32,'Dieta de liquidos',3, 1);
-insert into  Ingresos  values ('Chalatenango','2021-09-03',3,33,'Alergico a la ibuprofeno',4, 2);
-insert into  Ingresos  values ('Chalatenango','2021-08-14',1,34,'Alergico a anticonvulsivos',5, 4);
-insert into  Ingresos  values ('Chalatenango','2021-08-11',4,35,'Dieta de liquidos',11, 5);
-insert into  Ingresos  values ('Chalatenango','2021-09-12',2,36,'Alergico al naproxeno',10, 11);
-insert into  Ingresos  values ('Chalatenango','2021-09-15',1,27,'Alergico a anticonvulsivos',7, 3);
+insert into  Ingresos  values ('Chalatenango','2021-09-08',3,23,'Alergico a la ibuprofeno',5, 5);
+insert into  Ingresos  values ('Chalatenango','2021-09-09',1,22,'Alergico a anticonvulsivos',6, 6);
+insert into  Ingresos  values ('Chalatenango','2021-09-10',4,10,'Dieta de liquidos',7, 7);
+insert into  Ingresos  values ('Chalatenango','2021-09-11',1,16,'Alergico a la ibuprofeno',8, 8);
+insert into  Ingresos  values ('Chalatenango','2021-09-14',6,17,'Alergico al naproxeno',9, 9);
+insert into  Ingresos  values ('Chalatenango','2021-09-24',1,10,'Alergico a anticonvulsivos',10, 10);
+insert into  Ingresos  values ('Chalatenango','2021-08-04',5,30,'Dieta de liquidos',11, 11);
+insert into  Ingresos  values ('Chalatenango','2021-09-01',1,31,'Alergico a anticonvulsivos',12, 10);
+insert into  Ingresos  values ('Chalatenango','2021-09-02',1,32,'Dieta de liquidos',13, 1);
+insert into  Ingresos  values ('Chalatenango','2021-09-03',3,33,'Alergico a la ibuprofeno',14, 2);
+insert into  Ingresos  values ('Chalatenango','2021-08-14',1,34,'Alergico a anticonvulsivos',15, 4);
+insert into  Ingresos  values ('Chalatenango','2021-08-11',4,35,'Dieta de liquidos',16, 5);
+insert into  Ingresos  values ('Chalatenango','2021-09-12',2,36,'Alergico al naproxeno',17, 11);
+
 
 select * from Ingresos;
+
+/*--------------------------------Tabla medicos--------------------------------*/
+
 
  create table Medicos(
  CodigoIdentificacionMedico int primary Key identity (1,1)not null,
  Nombre varchar(30) not null,
  Apellido varchar(30) not null,
- Especialidad int foreign key (Especialidad) references Especialidades(idEspecialidades) not null,
  NumeroColegiado int unique not null,
- cargo varchar(30) not null
+ fk_Especialidad int foreign key (fk_Especialidad) references Especialidades(idEspecialidades) not null,
+ fk_Cargo int foreign key (fk_Cargo) references  InformacionMedico (Codigo) not null,
  
  );
 
- insert into  Medicos  values ('Manuel','Diaz',1, 12345, 'Medico residente ');
- insert into  Medicos  values ('Marcos','Monterrosa',2, 12335, 'Medico responsable ');
- insert into  Medicos  values ('Matias','Montalvo',4, 12385, 'Medico especialista ');
- insert into  Medicos  values ('Carlos','Castillo',3, 12305, 'Medico asistente ');
- insert into  Medicos  values ('Jonathan','Mendez',6, 12315, 'Medico residente ');
- insert into  Medicos  values ('Gabriel','Menjivar',5, 12645, 'Medico asistente ');
- insert into  Medicos  values ('Jose','Sanchez',8, 12845, 'Medico especialista ');
- insert into  Medicos  values ('Adonay','Martinez',7, 10345, 'Medico responsable ');
+ insert into  Medicos  values ('Manuel','Diaz', 12345,1,2);
+ insert into  Medicos  values ('Marcos','Monterrosa', 12335,2,1);
+ insert into  Medicos  values ('Matias','Montalvo', 12385,3,3);
+ insert into  Medicos  values ('Carlos','Castillo', 12305,4,4);
+ insert into  Medicos  values ('Jonathan','Mendez', 12315,5,1);
+ insert into  Medicos  values ('Gabriel','Menjivar', 12645,6,2);
+ insert into  Medicos  values ('Jose','Sanchez', 12845,7,3);
+ insert into  Medicos  values ('Adonay','Martinez', 10345,8,4);
 
  select * from Medicos;
  
- /*drop table Especialidades*/
+ /*--------------------------------------Table Especialidades---------------------------------*/
  create table Especialidades(
  idEspecialidades int primary key identity(1,1),
  Especialidad varchar(50) not null
@@ -134,6 +150,17 @@ insert into  Especialidades values ('Neurocirugia');
 insert into  Especialidades values ('Urologia');
 
 select *  from Especialidades
+drop table Especialidades
 
+/*-------------------------------------Tabla InformacionMedico-------------------------------------*/
 
-/*drop table Pacientes*/
+create table InformacionMedico(
+Codigo int primary Key identity(1,1),
+Cargo varchar(50) not null);
+
+insert into InformacionMedico values ('Medico residente ');
+insert into InformacionMedico values ('Medico responsable ');
+insert into InformacionMedico values ('Medico especialista ');
+insert into InformacionMedico values ('Medico asistente ');
+
+select * from InformacionMedico;
